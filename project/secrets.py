75943820311b1c60c13498cbff6b4d53200858b9
Regina Aiken <92677444+rmaiken@users.noreply.github.com>
2022-02-02 17:57:58 +0000

    Initial commit

---
+++ project/secrets.py
@@ -0,0 +1,16 @@
+#AWS user credentials configuration
+username = 'aicore-group3'
+access_key_id = 'AKIA22P3VS4VZH5TYLPZ'
+secret_access_key = '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'
+region = 'eu-west-2'
+
+
+#upload to RDS
+database_type = 'postgresql'
+dbapi = 'psycopg2'
+endpoint = 'ikea-item-info.cvuwm7un4cd6.us-east-1.rds.amazonaws.com' # Change it for your AWS endpoint
+user = 'postgres'
+password = 'aicoreGroup3'
+port = 5432
+database = 'postgres'
+        
